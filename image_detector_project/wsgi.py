d39e120b8d4975e6d3ade864372167ab11013e7a
Denis Kalala <kalaladm@mail.uc.edu>
2025-10-28 14:36:29 -0400

    Fix duplicate docstring in wsgi.py

--- image_detector_project/wsgi.py
+++ image_detector_project/wsgi.py
@@ -7,15 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-"""
-WSGI config for image_detector_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
